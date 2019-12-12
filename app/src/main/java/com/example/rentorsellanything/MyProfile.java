98301d74a69ad42882932c7969971fb66bdc93e4
Aneeta <aneetaalex40@gmail.com>
2019-12-12 12:22:13 +0530

    Commit 12/12/2019

--- app/src/main/java/com/example/rentorsellanything/MyProfile.java
+++ app/src/main/java/com/example/rentorsellanything/MyProfile.java
@@ -1,9 +1,12 @@
 package com.example.rentorsellanything;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
@@ -21,7 +24,34 @@ public class MyProfile extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        final View rootView = inflater.inflate(R.layout.my_profile, container, false);
+
+        final TextView textView = (TextView)rootView.findViewById(R.id.tvEditProfile);
+        final TextView textView1 = (TextView)rootView.findViewById(R.id.tvMyPosts);
+        final TextView textView2 = (TextView)rootView.findViewById(R.id.tvWishlist);
+        final LinearLayout linearLayout =(LinearLayout)rootView.findViewById(R.id.editProfile);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                linearLayout.setVisibility(View.VISIBLE);
+            }
+        });
+        textView1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentBlock = new Intent(v.getContext(), MyActivity.class);
+                startActivity(intentBlock);
+            }
+        });
+        textView2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentBlock = new Intent(v.getContext(), MyActivity.class);
+                startActivity(intentBlock);
+            }
+        });
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.my_profile, container, false);
+        return rootView;
     }
 }
--- app/src/main/java/com/example/rentorsellanything/UserProfileActivity.java
+++ app/src/main/java/com/example/rentorsellanything/UserProfileActivity.java
@@ -6,12 +6,34 @@ import android.view.View;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class UserProfileActivity extends AppCompatActivity {
+
+    MyRecyclerViewAdapter adapter;
 
     public void onCreate(@Nullable Bundle x) {
         super.onCreate(x);
         setContentView(R.layout.activity_userprofile);
+
+        ArrayList<String> values = new ArrayList<>();
+        values.add("Data1");
+        values.add("Data2");
+        values.add("Data3");
+        values.add("Data4");
+        values.add("Data5");
+
+        final RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new MyRecyclerViewAdapter(this, values);
+
+        recyclerView.addItemDecoration(new DividerItemDecoration(recyclerView.getContext(), DividerItemDecoration.VERTICAL));
+        //adapter.setClickListener(this);
+        recyclerView.setAdapter(adapter);
     }
     public void processHomeBtnClick(View view)
     {
